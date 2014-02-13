3ceaaad26b2364a24f95a630bfed4010e9088b9a
Petar Marinov <pet1r.marinov@yahoo.com>
2014-02-13 19:00:19 +0200

    creating users

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,21 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Newsfeed::Application.config.secret_token = '8f3d60462cb4109f93e2fac7df477a6ea2ce6e36856eb315d5fb3f72d93ef90b1e37f2ed49fb9ee86db13b780e092db0b2081157260f814e5faac51e19917594'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Newsfeed::Application.config.secret_token = secure_token
+
+# Newsfeed::Application.config.secret_token = '8f3d60462cb4109f93e2fac7df477a6ea2ce6e36856eb315d5fb3f72d93ef90b1e37f2ed49fb9ee86db13b780e092db0b2081157260f814e5faac51e19917594'
