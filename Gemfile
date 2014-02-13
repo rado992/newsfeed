e3d7b610de8d243a2bccb8b60e0b932c5ae13f3c
Radoslav Varbanov <radvarbanov@gmail.com>
2014-02-13 21:02:15 +0200

    Working AES 256 encryption for user passwords

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ group :assets do
 end
 
 gem 'jquery-rails'
+#gem 'mongoid-encrypted-fields'
+gem 'gibberish'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,12 @@
 class User
   include Mongoid::Document
   has_many :messages
-  # include BCrypt
-  # include ActiveModel::SecurePassword
   before_create :create_remember_token
-  #validate :passwords_same
   field :name, type: String
+  field :password, type: String
   validates :name, presence: true, length: {maximum: 50}, uniqueness: true
-  #validates_presence_of :password, :password_confirmation
-  # uniqueness: {case_sensitive: false}
-  # index({ ssn: 1 }, { unique: true, name: "ssn_index" })
-  # include ActiveModel::SecurePassword
   field :password_digest, type: String
   field :remember_token, type: String
-  # validates :password_digest, presence: true, length: {minimum: 5}
-  # has_secure_password
   attr_accessible :name, :password, :password_confirmation
   def User.new_remember_token
     SecureRandom.urlsafe_base64
@@ -24,13 +16,22 @@ class User
     Digest::SHA1.hexdigest(token.to_s)
   end
 
-  private
+  public
+
+  @@cipher = Gibberish::AES.new("p4ssw0rd")
 
   def create_remember_token
     self.remember_token = User.encrypt(User.new_remember_token)
+
   end
 
-  def passwords_same
-    password == password_confirmation
+  def encrypt
+    encoded = @@cipher.enc(password)
+    update_attributes(password: encoded)
+  end
+
+  def decrypt
+    decoded = @@cipher.dec(password)
+    update_attributes(password: decoded)
   end
 end
