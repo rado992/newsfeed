8f3d0ca6839431b5819e3b94ccb6afb6d25c9439
Petar Marinov <pet1r.marinov@yahoo.com>
2014-02-14 19:44:59 +0200

    Block/Unblock users.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
   def create
     user = User.find_by(name: params[:session][:name])
-    if user # && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_to root_path
     else
       flash.now[:error] = 'Invalid name/password combination'
       render 'new'
